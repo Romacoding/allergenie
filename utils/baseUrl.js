16a9c3fedf1fb0f8eb14db5976f5a1aa20ce17ff
Roman Ostash <roman.ostash@stots.edu>
2020-08-26 22:17:23 -0400

    minor changes

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "production"
-    ? "https://aller-genie.herokuapp.com"
-    : "http://localhost:3000";
+const baseUrl = process.env.NODE_ENV === "production" ? "https://aller-genie.herokuapp.com" : "http://localhost:3000";
 
 export default baseUrl;
