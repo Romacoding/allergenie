1b1d93d3c7e2d9b3796f39cf8eb24c4754332f05
Roman Ostash <roman.ostash@stots.edu>
2020-09-01 21:56:59 -0400

    domain change

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://allergenie.herokuapp.com"
+    ? "https://aller-genie.herokuapp.com"
     : "http://localhost:3000";
 
 export default baseUrl;
